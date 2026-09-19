d18aa66c4f976ce0b423c2dfc9949342b62a3259
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2026-09-19 04:16:32 +0000

    add new section for

--- public/views/index.php
+++ public/views/index.php
@@ -1,3 +1,10 @@
-<?php $engine->include( 'header' ); ?>
-<?php $engine->include( 'content' ); ?>
-<?php $engine->include( 'footer' ); ?>
+<?php
+
+// Loads header/*.php template
+$engine->include( 'header', $hierarchy );
+
+// Loads content/*.php template
+$engine->include( 'content', $hierarchy );
+
+// Loads footer/*.php template
+$engine->include( 'footer', $hierarchy );
